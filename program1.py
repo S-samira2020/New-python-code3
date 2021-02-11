9a3567533fd994a0555e009c6782718a85282779
Samira <54632872+S-samira2020@users.noreply.github.com>
2021-02-12 01:44:42 +0600

    Add files via upload

---
+++ program1.py
@@ -0,0 +1,11 @@
+is_hot =  False
+is_cold = True
+if is_hot:
+    print('it is a hot day')
+    print('drink plenty of water\n')
+elif is_cold:
+    print('it is a cold day')
+    print('wear your clothes\n')
+else:
+    print('it is a lovely day')
+print('enjoy your day')
